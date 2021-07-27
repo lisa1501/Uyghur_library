911debe7148e0c1af39d063cb3c407573bc6c818
halisa <halisasaipulaa@gmail.com>
2021-07-27 11:23:21 -0700

    working on Crud

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about', views.about, name='blog-about'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('about/', views.about, name='blog-about'),
 ] 
+
+#<app>/<model>_<viewtype>.html
