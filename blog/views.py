9f369975775d95a652e99d3bc0bdd53806231f71
hena <lee.henajane@gmail.com>
2021-07-27 15:41:00 -0500

    adds forms

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from typing import List
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 
 
@@ -18,8 +24,28 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
-    
+
+# view with a form when we create a new post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        # take author equal to the current logged in user
+        form.instance.author = self.request.user
+        # run form_valid method on our parent class, super()
+        # which would run anyway, BUT we want to override it 
+        # with the author which is the current logged in user
+        return super().form_valid(form)
 
 
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+        
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
